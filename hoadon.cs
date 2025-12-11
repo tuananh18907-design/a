1ff4347270621844291dda1d31cf6d07f7a44c76
tuananh18907-design <tuananh18907@gmail.com>
2025-12-11 12:13:46 +0700

    Add files via upload

---
+++ hoadon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace cafe.Module.cafe2
+{
+
+    public partial class hoadon
+    {
+        public hoadon(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
